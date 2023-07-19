739858017855dd3452df5fe0d635faa12533a254
Daviferlo12 <dflopez345@misena.edu.co>
2023-07-18 23:23:49 -0500

    Authenication

--- routers/jwt_authentication.py
+++ routers/jwt_authentication.py
@@ -1,20 +1,23 @@
 # FAST API
 from fastapi import FastAPI, Depends, HTTPException, status
-from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
+
+# PYTHON
+from pydantic import BaseModel
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 
-algorithm = "HS256"
+ALGORITHM = "HS256"
 
-acces_token_duration = 1
+acces_token_duration = 2
+secret = "0b69c4486093953c50403bd4c7c2ba3b1007c630059f6783700bd946bf1bed32"
 
 app = FastAPI()
 
 
-oauth2 = OAuth2PasswordBearer(tokenUrl='Login')
+oauth2 = OAuth2PasswordBearer(tokenUrl='login')
 
 crypt = CryptContext(schemes=["bcrypt"])
 
@@ -33,14 +36,14 @@ users_db = {
         'full_name' : 'David Lopez',
         'email' : 'daviferlo@gmail.com',
         'disable' : False,
-        'password' : '$2a$12$c5X0dKFEZQQTs82NvhO3I.tLL3wdGvWfE4fpKdIfgezHkV.PuXpVS'
+        'password' : '$2a$12$OksUPjFTZLVwyUD8HaGNxO2j2iTZrGqi9a4PiopreVPdK2iQQ3Ffy'
      },
     "daviferlo12" : {
         'username' : 'daviferlo12',
         'full_name' : 'David Lopez 2',
         'email' : 'daviferlo@gmail.com',
         'disable' : True,
-        'password' : '$2a$12$vJUvSW8FhblMzy2zkMmm4.dyqwCwcnSmbwEOGGhjxslaKcv5ZCUaq'
+        'password' : '$2a$12$bOnZGaUXc2yQT9q5N9zBmOlgH2dttDS/6QaKV7Ue6HmF5i9UG1GO6'
      }
 }
 
@@ -48,6 +51,49 @@ def search_user_db(username : str):
     if username in users_db:
         return User_DB(**users_db[username])
     
+def search_user(username : str):
+    if username in users_db:
+        return User(**users_db[username])
+   
+
+
+async def auth_user(token : str = Depends(oauth2)):
+    
+    exeption = HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail= "ERROR: invalid credentials...",
+                headers={"WWW-Authenticate" : "Bearer"}
+            )
+    try:
+        username = jwt.decode(token, secret, algorithms = [ALGORITHM]).get("sub")
+        
+        if username is None:
+            raise HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail= "ERROR: invalid credentials... 1",
+                headers={"WWW-Authenticate" : "Bearer"}
+            )
+        
+    except JWTError:
+        raise HTTPException(
+                status_code= status.HTTP_401_UNAUTHORIZED,
+                detail= "ERROR: invalid credentials... 2",
+                headers={"WWW-Authenticate" : "Bearer"}
+            )
+    
+    return search_user(username)
+
+
+    
+async def current_user(user : User = Depends(auth_user)):
+        
+    if user.disable:
+        raise HTTPException(
+            status_code= status.HTTP_400_BAD_REQUEST,
+            detail= "ERROR: user disabled..."
+        )
+    
+    return user 
     
 @app.post(path='/login')
 async def login(form : OAuth2PasswordRequestForm = Depends()):
@@ -71,5 +117,11 @@ async def login(form : OAuth2PasswordRequestForm = Depends()):
         "exp" : datetime.now() + timedelta(minutes=acces_token_duration)
     }
         
-    return {'access_token' : acces_token, 'token_type': 'bearer'}
+    return {'access_token' : jwt.encode(acces_token, secret, algorithm = ALGORITHM), 'token_type': 'bearer'}
+
+
+
+@app.get(path="/users/me")
+async def me(user : User = Depends(current_user)):
+    return user
 
--- tests.py
+++ tests.py
@@ -1,24 +1,8 @@
+from datetime import datetime, timedelta
+import time
 
-
-users_db = {
-     "daviferlo" : {
-        'username' : 'daviferlo',
-        'full_name' : 'David Lopez',
-        'email' : 'daviferlo@gmail.com',
-        'disable' : False,
-        'password' : '1234admin'
-     },
-    "daviferlo12" : {
-        'username' : 'daviferlo12',
-        'full_name' : 'David Lopez 2',
-        'email' : 'daviferlo@gmail.com',
-        'disable' : True,
-        'password' : '123445rer'
-     }
-}
-
-
-print(**users_db['daviferlo12'])
-        
+print(datetime.utcnow())
+print(datetime(1970, 1, 1) + timedelta(seconds=time.time()))
+print(datetime(*time.gmtime()[:6]))
 
 
